b8998a8e4d86553f3ee7135a3642cb2398e631f6
Bruno <onyedika.okafor@jumia.com>
2020-07-27 14:38:53 +0100

    Project init

---
+++ src/main/java/com/codeserver/CodeserverApplication.java
@@ -0,0 +1,13 @@
+package com.codeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeserver/CodeserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
